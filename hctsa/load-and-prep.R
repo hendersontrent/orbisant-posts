68415a48acdcc3d58cef6fef3d284bd286f43db1
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-04-05 22:13:42 +1000

    More robust analysis and new plots

--- hctsa/load-and-prep.R
+++ hctsa/load-and-prep.R
@@ -14,7 +14,7 @@ library(readxl)
 library(data.table)
 library(catch22)
 library(caTools)
-library(brms)
+library(rstan)
 library(bayesplot)
 library(Cairo)
 library(broom)
@@ -222,13 +222,21 @@ normed <- outs %>%
   dplyr::select(-c(iso_code, continent_name)) %>%
   dplyr::select(c(country, names, values)) %>%
   dplyr::group_by(names) %>%
-  dplyr::mutate(values = normalise_catch(values, method = "z-score")) %>%
+  dplyr::mutate(values = normalise_catch(values, method = "RobustSigmoid")) %>%
   dplyr::ungroup() %>%
   tidyr::drop_na()
 
 # Calculate correlations
 
+countrs_to_keep <- normed %>%
+  group_by(country) %>%
+  summarise(counter = n()) %>%
+  filter(counter == 22)
+
+countrs_to_keep <- countrs_to_keep$country
+
 cor_dat <- normed %>%
+  filter(country %in% countrs_to_keep) %>%
   tidyr::pivot_wider(id_cols = "names", names_from = "country", values_from = "values") %>%
   dplyr::select(-c(names))
 
@@ -238,7 +246,7 @@ result <- cor(cor_dat)
 
 melted <- reshape2::melt(result)
 
-CairoPNG("hctsa/output/cormat.png",1100,1000)
+CairoPNG("hctsa/output/cormat.png",1500,1500)
 pcor <- melted %>%
   ggplot(aes(x = Var1, y = Var2)) +
   geom_tile(aes(fill = value)) +
@@ -262,7 +270,7 @@ col.order <- hclust(dist(t(result)))$order # Hierarchical cluster on columns
 dat_new <- result[row.order, col.order] # Re-order matrix by cluster outputs
 cluster_out <- reshape2::melt(as.matrix(dat_new)) # Turn into dataframe
 
-CairoPNG("hctsa/output/cormat-clustered.png",1100,1000)
+CairoPNG("hctsa/output/cormat-clustered.png",1500,1500)
 pcorc <- cluster_out %>%
   ggplot(aes(x = Var1, y = Var2)) +
   geom_tile(aes(fill = value)) +
@@ -278,3 +286,93 @@ pcorc <- cluster_out %>%
         axis.text.x = element_text(angle = 90, hjust = 1))
 print(pcorc)
 dev.off()
+
+#-------------------- Statistical modelling --------------------
+
+# Create groups
+
+group_labs <- outs %>%
+  group_by(country, continent_name) %>%
+  summarise(counter = n()) %>%
+  ungroup() %>%
+  dplyr::select(c(country, continent_name))
+
+outs1 <- normed %>%
+  filter(country %in% countrs_to_keep) %>%
+  inner_join(group_labs, by = c("country" = "country")) %>%
+  mutate(cont_group = ifelse(continent_name == "Asia", 1,0)) %>%
+  dplyr::select(-c(continent_name)) %>%
+  pivot_wider(id_cols = c(country, cont_group), names_from = names, values_from = values)
+
+options(mc.cores = parallel::detectCores()) # Parallel processing
+
+# Set up data for Stan
+
+name_list <- as.vector(colnames(outs1))
+name_list <- name_list[!name_list %in% c("country", "cont_group")]
+
+X <- outs1 %>%
+  dplyr::select(-c(country, cont_group))
+
+stan_data <- list(N = nrow(X),
+                  P = ncol(X),
+                  K = as.integer(length(name_list)),
+                  y = outs1$cont_group,
+                  X = as.matrix(X))
+
+# Fit a classification model
+
+m1 <- rstan::stan(file = "/Users/trenthenderson/Documents/Git/sawlog/inst/stan/BayesGLM.stan", 
+                  data = stan_data, iter = 3000, chains = 3, seed = 123, control = list(max_treedepth = 10))
+
+#------------------
+# Model Diagnostics
+#------------------
+
+bayesplot::color_scheme_set("red")
+
+# Posterior predictive checks
+
+bayesplot::ppc_bars(m1) +
+  ggplot2::labs(title = "Posterior predictive check",
+                x = "Group",
+                y = "Count")
+
+bayesplot::ppc_ecdf_overlay(m1) +
+  ggplot2::labs(title = "Posterior predictive check of cumulative probability function",
+                x = "Group",
+                y = "Cumulative Probability")
+
+# Chain convergence
+
+bayesplot::color_scheme_set("mix-blue-red")
+bayesplot::mcmc_trace(m1, regex_pars = c("beta"))
+
+# LOO-CV
+
+loo1 <- loo::loo(m1, save_psis = TRUE)
+plot(loo1)
+
+# Coefficients
+
+bayesplot::color_scheme_set("red")
+
+bayesplot::mcmc_hist(m1, regex_pars = c("beta")) +
+  ggplot2::labs(title = "Coefficient posterior distributions") +
+  ggplot2::geom_vline(xintercept = 0, lty = "dashed", colour = "black", size = 1)
+
+# Overall model summary with 90% credible intervals
+
+m1_dat <- as.data.frame(m1) %>%
+  dplyr::select(dplyr::contains("beta"))
+
+name_list_stan <- as.vector(colnames(m1_dat))
+
+m1_dat_re <- m1_dat %>%
+  dplyr::rename_at(vars(dplyr::all_of(name_list_stan)), ~ name_list) %>%
+  tidyr::pivot_longer(tidyr::everything(), names_to = "names", values_to = "values") %>%
+  dplyr::group_by(names) %>%
+  dplyr::summarise(lower = quantile(values, prob = 0.1),
+                   median = median(values),
+                   upper = quantile(values, prob = 0.9)) %>%
+  dplyr::ungroup()
